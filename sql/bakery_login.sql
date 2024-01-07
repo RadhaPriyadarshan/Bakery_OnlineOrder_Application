fc7f2b43422f01960426666a29f71ed06561310d
Radha priyadarsan <radhapriyan786@gmail.com>
2024-01-08 01:26:21 +0530

    Final commit

--- sql/bakery_login.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM bakery.bakery_login;
--- sql/bakery_purchase.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM bakery.bakery_purchase;
-SELECT *,
-blackforest * 50 AS blackforest, 
-cheese * 50 AS cheese ,
-strawberry * 50 AS strawberry ,
-blueberry * 50 AS blueberry ,
-blackforest * 50 + cheese * 50 + strawberry * 50 + blueberry * 50 AS total FROM bakery_purchase;
-
