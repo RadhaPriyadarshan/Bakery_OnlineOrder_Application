171b05dea358dad987f86ed2c61ca6ab7a8752d8
Radha priyadarsan <70469801+RadhaPriyadarshan@users.noreply.github.com>
2024-01-08 00:59:01 +0530

    Final Commit

---
+++ sql/bakery_login.sql
@@ -0,0 +1,1 @@
+SELECT * FROM bakery.bakery_login;
---
+++ sql/bakery_purchase.sql
@@ -0,0 +1,8 @@
+SELECT * FROM bakery.bakery_purchase;
+SELECT *,
+blackforest * 50 AS blackforest, 
+cheese * 50 AS cheese ,
+strawberry * 50 AS strawberry ,
+blueberry * 50 AS blueberry ,
+blackforest * 50 + cheese * 50 + strawberry * 50 + blueberry * 50 AS total FROM bakery_purchase;
+
